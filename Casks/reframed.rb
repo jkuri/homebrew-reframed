b13874f1a8f499145f633e854c3d2efa2be350ec
Jan Kuri <jkuri88@gmail.com>
2026-02-19 12:44:51 +0100

    update reframed to v0.9.2

--- Casks/reframed.rb
+++ Casks/reframed.rb
@@ -1,6 +1,6 @@
 cask "reframed" do
-  version "0.9.1"
-  sha256 "311ff438c700b9f652f0d71b17f677bfe391fdee682e237f6d3266d93fbee7aa"
+  version "0.9.2"
+  sha256 "366cb5b03fd3f5c7bbd6ad734bdfa8936ca7558d5b7969725d8d28bf60ef4440"
 
   url "https://github.com/jkuri/Reframed/releases/download/v#{version}/Reframed-#{version}.dmg"
   name "Reframed"
